46a9a5ee3d5155db816347d2117f11c301cccee0
midi2154 <midi2154@gmail.com>
2026-05-12 11:17:25 +0100

    Update render.go

--- render.go
+++ render.go
@@ -1,1 +1,18 @@
+package main
 
+import "strings"
+
+func RenderLine(text string, banner map[rune][]string) []string {
+	ouput := []string{}
+
+	for row := 0; row < 8; row++ {
+		var result strings.Builder
+		for _, char := range text {
+
+			line := banner[char][row]
+			result.WriteString(line)
+		}
+		ouput = append(ouput, result.String())
+	}
+	return ouput
+}
