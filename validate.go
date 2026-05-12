498f92872a9c3a7912d6423edcbd0a53794df542
midi2154 <midi2154@gmail.com>
2026-05-12 11:18:45 +0100

    Update validate.go

--- validate.go
+++ validate.go
@@ -1,1 +1,12 @@
+package main
 
+import "fmt"
+
+func ValidateInput(input string) (rune, error) {
+	for _, char := range input {
+		if char < 32 || char > 126 {
+			return char, fmt.Errorf("Invalid character: %q", char)
+		}
+	}
+	return 0, nil
+}
